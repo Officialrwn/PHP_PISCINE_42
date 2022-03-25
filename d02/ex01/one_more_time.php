74edd27d6f54bac72dfccdda3d7ee3cb07e7e47d
leotran <leotran@student.hive.fi>
2022-03-25 12:09:35 +0200

    started on ex01

--- d02/ex01/one_more_time.php
+++ d02/ex01/one_more_time.php
@@ -1,11 +1,28 @@
-#!/usr/local/bin/php
+#!/usr/bin/php
 <?php
+	$weekdays = array("lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche");
+	$months = array(
+		"Jan" => "janvier",
+		"Feb" => "février",
+		"Mar" => "mars",
+		"Apr" => "avril",
+		"May" => "mai",
+		"Jun" => "juin",
+		"Jul" => "juillet",
+		"Aug" => "août",
+		"Sep" => "septembre",
+		"Oct" => "octobre",
+		"Nov" => "novembre",
+		"Dec" => "décembre",
+	);
+	$array = explode(" ", $argv[1]);
+	if (count($array) == 5)
+	{
+		$day = array_search(lcfirst($array[0]), $weekdays, 1);
+		$month = array_search(lcfirst($array[2]), $months, 1);
+	}
+
 /* 	$str = 'Tue Nov 12 2013 12:02:21';
 	$timestamp = strtotime($str);
 	echo $timestamp; */
-	//1384214400
-	//1384254141
-	$ts = 1384254141;
-	$date = new DateTime("@$ts");
-	echo $date->format('U = Y-m-d H:i:s');
 ?>
