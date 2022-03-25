abe8261e8d057c42378b078065ef08aacff42cf6
leotran <leotran@student.hive.fi>
2022-03-25 15:00:58 +0200

    miissing time zone ex01

--- d02/ex01/one_more_time.php
+++ d02/ex01/one_more_time.php
@@ -15,14 +15,27 @@
 		"Nov" => "novembre",
 		"Dec" => "décembre",
 	);
-	$array = explode(" ", $argv[1]);
-	if (count($array) == 5)
-	{
-		$day = array_search(lcfirst($array[0]), $weekdays, 1);
-		$month = array_search(lcfirst($array[2]), $months, 1);
-	}
-
-/* 	$str = 'Tue Nov 12 2013 12:02:21';
-	$timestamp = strtotime($str);
-	echo $timestamp; */
+		if ($argc == 2)
+		{
+			$array = explode(" ", $argv[1]);
+			$timepattern = "/(^0[0-9]$|1[0-9]|2[0-3]):(0[0-9]|[0-5][0-9]):(0[0-9]|[0-5][0-9])$/";
+			$datepattern = "/(^[1-9]$|^1[0-9]$|^2[0-9]$|^3[0-1])$/";
+			$check = 0;
+			if (count($array) == 5)
+			{
+				$check += preg_match($datepattern, $array[1], $date);
+				$check += preg_match($timepattern, $array[4], $time);
+				$day = array_search(lcfirst($array[0]), $weekdays, 1);
+				$month = array_search(lcfirst($array[2]), $months, 1);
+				$year = intval($array[3]);
+			}
+			if ($check == 2 && $day != NULL && $month != NULL && $year >= 1970 && $year <= time())
+			{
+				$str = "$month $date[0] $year $time[0]";
+				$timestamp = strtotime($str);
+				echo $timestamp;
+			}
+			else
+				echo "Wrong Format\n";
+		}
 ?>
