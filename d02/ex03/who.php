f2fc1f0f461aa0c1a5e285e7f5bf2a1e8493d10f
leotran <leotran@student.hive.fi>
2022-03-28 16:00:31 +0300

    minor fix

--- d02/ex03/who.php
+++ d02/ex03/who.php
@@ -1,9 +1,8 @@
 <?php
-	$str = implode("", file("/var/run/utmpx"));
-	//print_r(unpack("C*", $str));
-/* 	var_dump($str);
-	$pattern = "/ttys000(.*?)ksad/";
-	preg_match($pattern, $str[2], $m); */
-	//print_r($m[1]);
-	print_r(unpack("a256user/a4id/a32line/ipid/itype/I2time/a256host/i16pad", $str));
+	if ($f = fopen("/var/run/utmpx", "r")) {
+
+		while ($line = fread($f, 628)) {
+			print_r(unpack("a256user/a4id/a32line/ipid/itype/I2time/a256host/i16pad", $line));
+		}
+	}
 ?>
