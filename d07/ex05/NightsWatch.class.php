5ef1739fe64bf819afd0112b37bf603b1ca67889
leotran <leotran@student.hive.fi>
2022-04-02 16:08:54 +0300

    addadad

--- d07/ex05/NightsWatch.class.php
+++ d07/ex05/NightsWatch.class.php
@@ -9,12 +9,10 @@ class NightsWatch implements IFighter {
 	}
 	public function fight() {
 	}
-	
+
 	public function recruit($name) {
-		if (get_class($name) == 'JonSnow')
-			$this->fight();
-		else
-			echo "test\n";
+		if ($this->isABastard())
+			echo "test"; //$name->fight();
 	}
 }
 
