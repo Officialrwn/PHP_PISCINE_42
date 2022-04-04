047d38478c20a3225ed44c65e6597f4c14bef688
Leo MPB <leotran@student.hive.fi>
2022-04-04 22:02:36 +0300

    progress on d00/ex01

--- d07/ex05/NightsWatch.class.php
+++ d07/ex05/NightsWatch.class.php
@@ -1,18 +1,12 @@
 <?php
 
 class NightsWatch implements IFighter {
-	protected function isABastard() {
-		return False;
-	}
-	protected function makeHisFatherProud() {
-		return True;
-	}
 	public function fight() {
 	}
 
 	public function recruit($name) {
-		if ($this->isABastard())
-			echo "test"; //$name->fight();
+		if ($name instanceof IFighter)
+			$name->fight();
 	}
 }
 
